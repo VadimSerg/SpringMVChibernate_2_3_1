ebedfbbdea24d5f08d2cdcbe0530e6d2c9690176
VaDIM <33179761+VadimSerg@users.noreply.github.com>
2021-07-09 21:58:19 +1000

    Commit

--- src/main/java/web/config/AppInitializer.java
+++ src/main/java/web/config/AppInitializer.java
@@ -9,8 +9,7 @@ import javax.servlet.Filter;
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[] { AppConfig.class,
-                WebConfig.class};
+        return new Class[] { WebConfig.class};
     }
 
     @Override
